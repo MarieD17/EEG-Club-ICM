81af21f24dbdaf2d002113ff081d7ea6bcab3e76
MarieD17 <marie.degrave@icm-institute.org>
2023-05-30 14:18:50 +0200

    Update EEG-Club Pipeline ERPCore MMN.py

--- Preprocessing/EEG-Club Pipeline ERPCore MMN.py
+++ Preprocessing/EEG-Club Pipeline ERPCore MMN.py
@@ -27,14 +27,15 @@ from mne_icalabel import label_components
 if 'thandrillon' in os.getcwd():
     path_data='/Users/thandrillon/Data/ERPCore/ERPCore_N170/'
 elif 'degrave' in os.getcwd():
-    path_data='your_path'
+    path_data='/Users/marie.degrave/Documents/EEG-Club perso/MMN_ERP/'
 
 if os.path.exists(path_data+"reports")==False:
     os.makedirs(path_data+"reports")
 if os.path.exists(path_data+"intermediary")==False:
-    os.makedirs(path_data+"intermediary")
+    os.makedirs(path_data+"intermediary")                                       # create the directory if it doesn't exist 
     
-files = glob.glob(path_data + '*.set')
+files = glob.glob(path_data + '*.set')                                          # Return a list of pathnames matching the specified pattern
+
 
 # %% LOAD, FILTER, CLEAN
 report_Event = mne.Report(title='Auto Reject')
@@ -45,78 +46,85 @@ report_ICA = mne.Report(title='ICA')
 for file in files:
 
     # [1] LOAD RAW DATA
-    file_name=file.split('/')[-1]
-    sub_ID=file_name.split('_')[0]
-    report_prefix=path_data+"reports/"+sub_ID+"_"
-    raw = mne.io.read_raw_eeglab(file, preload=True)
-    raw_eeg = raw.copy().drop_channels(['HEOG_left','HEOG_right','VEOG_lower'])
+    file_name=file.split('/')[-1]                                               # Splits the file on / and return the last item (ex : 29_MMN.set)
+    sub_ID=file_name.split('_')[0]                                              # Splits the file on _ and return the first item (ex : 29)
+    report_prefix=path_data+"reports/"+sub_ID+"_"                               # Create a path
+    raw = mne.io.read_raw_eeglab(file, preload=True)                            # Read the EEGLAB .set file
+    raw_eeg = raw.copy().drop_channels(['HEOG_left','HEOG_right','VEOG_lower']) # New raw file removing some channels
     
     
     print(raw_eeg)
     #HCGSN256_montage = mne.channels.make_standard_montage('GSN-HydroCel-256')
     #raw.set_montage(HCGSN256_montage)
 
-    # [2] MONTAGE
-    montage = mne.channels.make_standard_montage('standard_1020')
-    raw_eeg.rename_channels(dict(FP1 = 'Fp1', FP2 = 'Fp2'))
-    raw_eeg.set_montage(montage, on_missing='ignore')
+
+    # [2] MONTAGE  -  Create a montage to maps channels to a layout
+    montage = mne.channels.make_standard_montage('standard_1020')               # Read a built-in standard montage that ships with MNE
+    raw_eeg.rename_channels(dict(FP1 = 'Fp1', FP2 = 'Fp2'))                     # Rename channels to match with the montage 
+    raw_eeg.set_montage(montage, on_missing='ignore')                           # Add it to the raw object, ignore when channels have missing coordinates
 
     
     # [3] REREFERNCING AND FILTERING
-    raw_eeg.resample(256)
-    sfreq = raw_eeg.info["sfreq"]
-    raw_eeg.set_eeg_reference("average")
-    raw_eeg.filter(0.1, 100, fir_design='firwin')
-    raw_eeg.notch_filter(60,
-                    filter_length='auto', phase='zero')
-    report = mne.Report(title=sub_ID)
-    report.add_raw(raw=raw_eeg, title='Filtered Cont from"raw_eeg"', psd=False)  # omit PSD plot
+    raw_eeg.resample(256)                                                       # Resample all channels to speed up computations
+    sfreq = raw_eeg.info["sfreq"]                                               # Get the sampling frequency used while recording
+    raw_eeg.set_eeg_reference("average")                                        # Set the reference for EEG
+    raw_eeg.filter(0.1, 100, fir_design='firwin')                               # Quick filtering with a linear filter (FIR)
+    raw_eeg.notch_filter(60, filter_length='auto', phase='zero')                # Remove electrical artifact
+    report = mne.Report(title=sub_ID)                                           # Create one report by subject
+    report.add_raw(raw=raw_eeg, title='Filtered Cont from"raw_eeg"', 
+                   psd=False)                                                   # Add raw object to the report, omit PSD plot
+
 
     # [4] EVENTS
-    stim_events=[range(1,40),range(41,80),range(101,140),range(141,180)]
-    # face, car, scrambled face, scrambled car
-    (events, event_dict) = mne.events_from_annotations(raw_eeg)
-    events=mne.merge_events(events, list(range(1,41)), 1001, replace_events=True)
+    stim_events=[range(1,40),range(41,80),range(101,140),range(141,180)]        # Recreate the events values : face, car, scrambled face, scrambled car
+
+    (events, event_dict) = mne.events_from_annotations(raw_eeg)                 # Get events and dictionary events from annotations, and merge the events
+    events=mne.merge_events(events, list(range(1,41)), 1001, replace_events=True)      
     events=mne.merge_events(events, list(range(41,81)), 1002, replace_events=True)
     events=mne.merge_events(events, list(range(101,141)), 1003, replace_events=True)
     events=mne.merge_events(events, list(range(141,181)), 1004, replace_events=True)
     
-    count_events=mne.count_events(events)
+    count_events=mne.count_events(events)                                       # Count the different types of events
     face_id=1001;
     car_id=1002;
-    stim_events = mne.pick_events(events, include=[face_id,car_id])
+    stim_events = mne.pick_events(events, include=[face_id,car_id])             # Select the events of interest
 
-    report.add_events(events=stim_events, title='Events from "stim_events"', sfreq=sfreq)
+    report.add_events(events=stim_events, 
+                      title='Events from "stim_events"', sfreq=sfreq)           # Add events to the HTML report
 
-    report_Event.add_events(events=stim_events, title='Events: '+sub_ID, sfreq=sfreq)
+    report_Event.add_events(events=stim_events, 
+                            title='Events: '+sub_ID, sfreq=sfreq)
+
 
     # [5] EPOCHS
     epochs = mne.Epochs(raw_eeg, events, event_id=[face_id,car_id],
-                        tmin=-0.2, tmax=1.2, reject=None, preload=True)
-    report.add_epochs(epochs=epochs, title='Epochs from "epochs"')
+                        tmin=-0.2, tmax=1.2, reject=None, preload=True)         # Extract epochs from raw, relative to time-locked events
+    report.add_epochs(epochs=epochs, title='Epochs from "epochs"')              # Add epochs to the report
     savename = "e_" + sub_ID + ".fif"
-    epochs.save(path_data+"intermediary/"+savename, overwrite=True)
+    epochs.save(path_data+"intermediary/"+savename, overwrite=True)             # Save the report
     
-    # [6] AUTOREJECT
-    n_interpolates = np.array([1, 4, 32])
-    consensus_percs = np.linspace(0, 1.0, 11)
+    
+    # [6] AUTOREJECT  -  automatically reject bad trials and repair bad channels
+    n_interpolates = np.array([1, 4, 32])                                       # Values to try for the number of channels for which to interpolate
+    consensus_percs = np.linspace(0, 1.0, 11)                                   # Values to try for perc of channels that must agree 
     ar = AutoReject(n_interpolates, consensus_percs,
-                    thresh_method='random_search', random_state=42)
-    ar.fit(epochs)
-    epochs_clean, reject_log = ar.transform(epochs, return_log=True)
+                    thresh_method='random_search', random_state=42)              
+    ar.fit(epochs)                                                              # Fit the epochs on the autoreject object
+    epochs_clean, reject_log = ar.transform(epochs, return_log=True)            # Remove bad epochs, repairs sensors and returns clean epochs
     epochs_clean.set_eeg_reference("average")
 
     savename = "ce_" + sub_ID + ".fif"
-    epochs_clean.save(path_data+"intermediary/"+savename, overwrite=True)
+    epochs_clean.save(path_data+"intermediary/"+savename, overwrite=True)       # Save the cleaned data
     
-    fig = reject_log.plot(orientation = 'horizontal', show=False)
+    fig = reject_log.plot(orientation = 'horizontal', show=False)               # Plot a resume of good, interpolate and bad channels and epochs
 
     report.add_figure(
         fig=fig,
         title="Reject log",
         caption="The rejct log returned by autoreject",
         image_format="PNG",
-    )
+    )                                                                           # Add the figure to the report
+    
     report_AR.add_figure(
         fig=fig,
         title="Reject log: "+sub_ID,
@@ -124,21 +132,34 @@ for file in files:
         image_format="PNG",
     )
     
-    # [8] ICA
-    ica_epochs = mne.Epochs(raw_eeg.copy().filter(l_freq=1.0, h_freq=None), events, event_id=[face_id,car_id],
-                        tmin=-0.2, tmax=1.2, reject=None, preload=True,baseline=None)
-    ica_epochs_clean = ar.transform(ica_epochs)
-    ica = mne.preprocessing.ICA(n_components=15, max_iter="auto", random_state=97,method='infomax', fit_params=dict(extended=True))
-    ica.fit(ica_epochs_clean)
-    savename = "ica_ce_" + sub_ID + ".fif"
-    ica.save(path_data+"intermediary/"+savename, overwrite=True)
     
-    # ICA rejection
-    ica_classification=label_components(ica_epochs_clean, ica, method='iclabel')
+    # [7] ICA
+    ica_epochs = mne.Epochs(raw_eeg.copy().filter(l_freq=1.0, h_freq=None), 
+                            events, event_id=[face_id,car_id], tmin=-0.2, 
+                            tmax=1.2, reject=None, preload=True,baseline=None
+                            )                                                   # Extracts the epochs
+    ica_epochs_clean = ar.transform(ica_epochs)                                 # Remove bad epochs, repairs sensors and returns clean epochs
+    ica = mne.preprocessing.ICA(n_components=15, max_iter="auto", 
+                                random_state=97,method='infomax', 
+                                fit_params=dict(extended=True)
+                                )                                               # Set the Independant Component Analysis parameters
+    ica.fit(ica_epochs_clean)                                                   # Run the ICA decomposition on raw data
+    
+    savename = "ica_ce_" + sub_ID + ".fif"
+    ica.save(path_data+"intermediary/"+savename, overwrite=True)                # Save ICA solution 
+    
+    
+        # ICA rejection
+    ica_classification=label_components(ica_epochs_clean, ica, 
+                                        method='iclabel'
+                                        )                                       # Create a dictionary with the probability of the classes for each independant component
+    
     ica_labels=pd.DataFrame(ica_classification)
-    ica_labels.to_csv(report_prefix+'ICAlabels.csv')
+    ica_labels.to_csv(report_prefix+'ICAlabels.csv')                            # Add the data structure to the report
+    
     labels = ica_labels["labels"]
-    exclude_idx = [idx for idx, label in enumerate(labels) if label not in ["brain", "other"]]
+    exclude_idx = [
+        idx for idx, label in enumerate(labels) if label not in ["brain", "other"]]
     caption_str=''
     for idx, label in enumerate(labels):
         caption_str=caption_str+'ICA'+str(idx)+': '+label+'; '
@@ -147,7 +168,10 @@ for file in files:
     epochs_clean_ica=ica.apply(epochs_clean, exclude=exclude_idx)
     epochs_clean_ica.set_eeg_reference("average")
 
-    ica_fig=ica.plot_sources(ica_epochs_clean, show_scrollbars=True, show=False)
+    ica_fig=ica.plot_sources(
+        ica_epochs_clean, show_scrollbars=True, show=False
+        )                                                                       # Plot component latent sources over epochs
+    
     report.add_ica(
         ica=ica,
         title='ICA cleaning',
@@ -172,8 +196,9 @@ for file in files:
         caption=caption_str,
     )
 
-    # [7] ERP
-    evoked_face  = epochs[str(face_id)].average()
+
+    # [8] ERP
+    evoked_face  = epochs[str(face_id)].average()                               # Compute an average over epochs
     evoked_car = epochs[str(car_id)].average()
     
     evoked_clean_face  = epochs_clean[str(face_id)].average()
@@ -187,18 +212,27 @@ for file in files:
     savename = "erp_ce_" + sub_ID + ".fif"
     mne.write_evokeds(path_data+"intermediary/"+savename, 
                       list(evoked_clean_perCond.values()), overwrite=True
-                     )
+                     )                                                          # Write the evoked dataset to a file
+    
     report.add_evokeds(
-        evokeds=[evoked_car,evoked_face,evoked_clean_car,evoked_clean_face,evoked_ica_clean_car,evoked_ica_clean_face],
-        titles=["car", "face","clean car", "clean face","ica+clean car", "ica+clean face"],  # Manually specify titles
+        evokeds=[evoked_car,evoked_face,evoked_clean_car,evoked_clean_face,
+                 evoked_ica_clean_car,evoked_ica_clean_face],
+        titles=["car", "face","clean car", "clean face",
+                "ica+clean car", "ica+clean face"],                             # Manually specify titles
         n_time_points=5,
-        replace=True)
+        replace=True
+        )
 
     
     # [9] CONTRAST
     picks = 'PO8'
-    evokeds_ica_clean = dict(car=evoked_ica_clean_face, face=evoked_ica_clean_car)
-    erp_ica_clean_fig=mne.viz.plot_compare_evokeds(evokeds_ica_clean, picks=picks, show=False)
+    evokeds_ica_clean = dict(
+        car=evoked_ica_clean_face, face=evoked_ica_clean_car
+        )                                                                       # Create a dictionary for the standard and deviant signal (MMN)
+    erp_ica_clean_fig=mne.viz.plot_compare_evokeds(
+        evokeds_ica_clean, picks=picks, show=False
+        )                                                                       # Plot the evoked time courses for the two conditions
+    
     evokeds = dict(car=evoked_face, face=evoked_car)
     erp_fig=mne.viz.plot_compare_evokeds(evokeds, picks=picks, show=False)
 
@@ -213,12 +247,21 @@ for file in files:
           caption="Face vs Car at PO8",
       )
  
-    face_ica_clean_vis = mne.combine_evoked([evoked_ica_clean_face, evoked_ica_clean_car], weights=[1, -1])
-    erp_ica_clean_but_fig=face_ica_clean_vis.plot_joint(show=False)
-    face_clean_vis = mne.combine_evoked([evoked_clean_face, evoked_clean_car], weights=[1, -1])
+    face_ica_clean_vis = mne.combine_evoked(
+        [evoked_ica_clean_face, evoked_ica_clean_car], weights=[1, -1]
+        )                                                                       # Merge evoked data
+    erp_ica_clean_but_fig=face_ica_clean_vis.plot_joint(show=False)             # Plot evoked data as butterfly plot and add topomaps for time points
+    
+    face_clean_vis = mne.combine_evoked(
+        [evoked_clean_face, evoked_clean_car], weights=[1, -1]
+        )
     erp_clean_but_fig=face_clean_vis.plot_joint(show=False)
-    face_vis = mne.combine_evoked([evoked_face, evoked_car], weights=[1, -1])
+    
+    face_vis = mne.combine_evoked(
+        [evoked_face, evoked_car], weights=[1, -1]
+        )
     erp_but_fig=face_vis.plot_joint(show=False)
+    
     report.add_figure(
           erp_but_fig,
           title="ERP contrast (butterfly)",
@@ -254,6 +297,7 @@ for file in files:
           caption="Face vs Car across all Elec",
       )
     
+    # Save all of the reports
     report.save(report_prefix+"pipeline.html", overwrite=True, open_browser=False)
     
     report_Event.save(path_data+"reports/"+"Events.html", overwrite=True, open_browser=False)
@@ -265,29 +309,28 @@ for file in files:
     
 # %% GET ERPs across subjects
 evokeds_files = glob.glob(path_data+"intermediary/" + '/erp_ce_*.fif')
-evokeds = {} #create an empty dict
+evokeds = {}                                                                    # create an empty dict
 conditions = ['1001','1002']
-# #convert list of evoked in a dict (w/ diff conditions if needed)
+
 for idx, c in enumerate(conditions):
     evokeds[c] = [mne.read_evokeds(d)[idx] for d in 
-    evokeds_files]
+    evokeds_files]                                                              # convert list of evoked in a dict (w/ diff conditions if needed)
 
-evokeds # We can see that he matched the conditions by treating each as if it was 2 objcts as before 
+evokeds                                                                         # We can see that he matched the conditions by treating each as if it was 2 objcts as before 
 
 
-# "Plot averaged ERP on all subj"
 ERP_mean = mne.viz.plot_compare_evokeds(evokeds,
                              picks='PO8', show_sensors='upper right',
                              title='Averaged ERP all subjects',
-                            )
+                            )                                                   # Plot averaged ERP on all subj
 plt.show()
 
 
-#gfp: "Plot averaged ERP on all subj"
+
 ERP_gfp = mne.viz.plot_compare_evokeds(evokeds,
                              combine='gfp', show_sensors='upper right',
                              title='Averaged ERP all subjects',
-                            )
+                            )                                                   #gfp: "Plot averaged ERP on all subj"
 plt.show()
 
 
